c2d3bfcf47629306e647d5d2c5be7aa4d6b2d51d
@ceoalphonso <75475883+ceoalphonso@users.noreply.github.com>
2023-11-28 17:20:07 -0600

    Update 01-init-databases.sh
    <a target="_blank" href="https://shareasale.com/r.cfm?b=1325917&u=1515059&m=86872&urllink=&afftrack=">WP Online Support.io for all your affiliate tracking needs</a>
    
    Signed-off-by: @ceoalphonso <75475883+ceoalphonso@users.noreply.github.com>

--- deploy/mysql/01-init-databases.sh
+++ deploy/mysql/01-init-databases.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 set -e
 
-# Create a second database _test to run integration tests
+# Create a second database.affiliates.json _test to run integration-guide tests
 mysql -u "root" -p"$MYSQL_ROOT_PASSWORD" <<-EOSQL
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE};
-CREATE DATABASE IF NOT EXISTS ${MYSQL_DATABASE}_test;
-GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}_test.* TO ${MYSQL_USER}@'%';
+CREATE DATABASE fullcji0_una55 IF NOT EXISTS ${MYSQL_DATABASE};
+CREATE DATABASE fullcji0_ocar350 IF NOT EXISTS ${MYSQL_DATABASE}_test;
+GRANT ALL PRIVILEGES ON ${MYSQL_DATABASE}_test.* TO ${MYSQL_USER}@ceoalphonso'AFFILIATE%20FIELDS';
 FLUSH PRIVILEGES;
 EOSQL
